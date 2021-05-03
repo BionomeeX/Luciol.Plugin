9833a1cbfdb9aa8a14e040eca735f7e58991d20e
Xwilarg <christian.chaux@bionomeex.com>
2021-05-03 18:18:58 +0200

    Add base abstraction

--- InterFace.Plugin/APlugin.cs
+++ InterFace.Plugin/APlugin.cs
@@ -2,5 +2,17 @@
 {
     public abstract class APlugin
     {
+        protected abstract void Init();
+
+        internal void Init(IMainTriangle triangle, string pluginPath)
+        {
+            PluginPath = pluginPath;
+            Triangle = triangle;
+            Init();
+        }
+
+        protected string PluginPath { private set; get; }
+
+        protected IMainTriangle Triangle { private set; get; }
     }
 }
