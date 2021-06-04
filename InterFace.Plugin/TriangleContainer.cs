64ad86ed4002d752e4476b94c1588a33ef39f4c5
Xwilarg <christian.chaux@bionomeex.com>
2021-06-04 11:07:15 +0200

    Add Triangle Container code

--- InterFace.Plugin/TriangleContainer.cs
+++ InterFace.Plugin/TriangleContainer.cs
@@ -1,8 +1,18 @@
 ﻿using Avalonia.Controls;
+using System;
 
 namespace InterFace.Plugin
 {
-    public class TriangleContainer : UserControl
+    public class TriangleContainer : DockPanel
     {
+        internal static Func<UserControl> ImplControlConstruction { set; private get; }
+
+        public override void EndInit()
+        {
+            if (ImplControlConstruction != null)
+            {
+                Children.Add(ImplControlConstruction());
+            }
+        }
     }
 }
