d069cf58288a1c11ccbd0cfdee0681b5313cd61f
Xwilarg <christian.chaux@bionomeex.com>
2021-06-29 11:50:28 +0200

    Add number input field

---
+++ InterFace.Plugin/Preference/NumberInputTextPreference.cs
@@ -0,0 +1,22 @@
+﻿using System.Linq;
+
+namespace InterFace.Plugin.Preference
+{
+    public class NumberInputTextPreference : InputTextPreference
+    {
+        public override string Value
+        {
+            get => _component.Text;
+            set
+            {
+                if (value.All(x => char.IsDigit(x)))
+                {
+                    _component.Text = value;
+                }
+            }
+        }
+
+        public NumberInputTextPreference(string name) : base(name)
+        { }
+    }
+}
