a285d888a16e25089bc6f3c44cef2e73dc68761b
Clément Carré <clement.carre@bionomeex.com>
2021-07-18 07:55:48 +0200

    Update IMainTriangle.cs

--- Luciol.Plugin/Context/IMainTriangle.cs
+++ Luciol.Plugin/Context/IMainTriangle.cs
@@ -26,6 +26,13 @@ namespace Luciol.Plugin.Context
         /// <param name="layer">Layer to check</param>
         /// <returns></returns>
         public bool DoesContainsAnnotation(int posX, int posY, int layer);
+        /// <summary>
+        /// Check if the given position is in the triangle
+        /// </summary>
+        /// <param name="posX">X position to check</param>
+        /// <param name="posY">Y position to check</param>
+        /// <param name="layer">Layer to check</param>
+        /// <returns>true if in the triangle, false otherwise</returns>
         public bool IsPositionValid(int posX, int posY, int layer);
         /// <summary>
         /// Get all annotations already placed
