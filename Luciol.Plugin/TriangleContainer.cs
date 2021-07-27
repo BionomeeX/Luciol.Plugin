9bd4d08f57d67919a1a6c80ed0260ce5cefc0f5b
Xwilarg <xwilarg@yahoo.fr>
2021-07-27 19:26:29 +0200

    Move data transformation in Luciol.Plugin

--- Luciol.Plugin/TriangleContainer.cs
+++ Luciol.Plugin/TriangleContainer.cs
@@ -1,18 +1,28 @@
 ﻿using Avalonia.Controls;
 using System;
+using System.Drawing;
 
 namespace Luciol.Plugin
 {
     public class TriangleContainer : DockPanel
     {
-        internal static Func<UserControl> ImplControlConstruction { set; private get; }
+        internal static Func<TriangleContainer, UserControl> ImplControlConstruction { set; private get; }
 
-        public override void EndInit()
+        public override sealed void EndInit()
         {
             if (ImplControlConstruction != null)
             {
-                Children.Add(ImplControlConstruction());
+                Children.Add(ImplControlConstruction(this));
             }
         }
+
+        /// <summary>
+        /// Apply a transformation on triangle data
+        /// </summary>
+        /// <param name="value">Current value of the point</param>
+        /// <param name="maxValue">Max value in the triangle</param>
+        /// <returns>Color to display</returns>
+        public virtual Color ValueTransformation(float value, float maxValue)
+            => Color.FromArgb(0, 0, (int)(Math.Abs(value) * 255 / maxValue));
     }
 }
