a5516687e681f7b428c9ffb4c1620daf65e326e6
Christian Chaux <christian.chaux@bionomeex.com>
2021-09-13 11:27:18 +0200

    Add manhattan colors preference

---
+++ Luciol.Plugin/Preference/ColorPalette.cs
@@ -0,0 +1,27 @@
+﻿using Avalonia.Controls;
+using Luciol.Plugin.Context;
+
+namespace Luciol.Plugin.Preference
+{
+    /// <summary>
+    /// Color palette preference, represents an array of color
+    /// </summary>
+    public class ColorPalettePreference : APreference<TextBlock, Color[]>
+    {
+        public ColorPalettePreference(string key, string name, Color[] defaultValue) : base(key, name, defaultValue)
+        { }
+
+        protected override Color[] ComponentValue
+        {
+            get => new[] { Color.Red, Color.Blue, Color.Green, Color.Magenta, Color.Cyan, Color.Yellow };
+            set { }
+        }
+
+        public override IControl GetComponent(Window window, IContext context)
+        {
+            var c = base.GetComponent(window, context); // We need to call that before because it init _component
+            _component.Text = "Preference not editable yet";
+            return c;
+        }
+    }
+}
--- Luciol.Plugin/Preference/GlobalSettings.cs
+++ Luciol.Plugin/Preference/GlobalSettings.cs
@@ -17,9 +17,11 @@ namespace Luciol.Plugin.Models.Preference
             new NumberInputTextPreference<float>("dragAndDropSensitivity", "Drag and Drop Sensitivity", 1f)
         });
 
-        public Settings Graph { get; } = new(new[]
+        public Settings Graph { get; } = new(new IPreferenceExport[]
         {
-            new ColorPreference("mainColor", "Main Graph Color", Color.Black)
+            new ColorPreference("mainColor", "Main Graph Color", Color.Black),
+            new ColorPalettePreference("manhattanColors", "Colors used for Manhattan graph",
+                new[] { Color.Red, Color.Blue, Color.Green, Color.Magenta, Color.Cyan, Color.Yellow })
         });
     }
 }
