07004d9240c3834d8fb33958d3c801320f681e2a
Xwilarg <xwilarg@yahoo.fr>
2021-10-19 10:10:17 +0200

    Add dependencies in APluginInfo

--- Luciol.Plugin/APluginInfo.cs
+++ Luciol.Plugin/APluginInfo.cs
@@ -4,12 +4,39 @@ namespace Luciol.Plugin
 {
     public abstract class APluginInfo
     {
+        /// <summary>
+        /// Name of the plugin
+        /// </summary>
         protected abstract string Name { get; }
+        /// <summary>
+        /// Name of the author/company who made the plugin
+        /// </summary>
         protected abstract string Author { get; }
+        /// <summary>
+        /// Reference to the type of your plugin
+        /// Probably typeof(YourPlugin)
+        /// </summary>
         protected abstract Type Plugin { get; }
+        /// <summary>
+        /// Description of what your plugin will do
+        /// </summary>
         public abstract string Description { get; }
+        /// <summary>
+        /// Version of your plugin
+        /// </summary>
         public abstract string Version { get; }
+        /// <summary>
+        /// Type of the plugin
+        /// </summary>
         public abstract PluginType PluginType { get; }
+
+        /// <summary>
+        /// If your plugin depends of others plugins to work properly
+        /// </summary>
+        public virtual Type[] Dependencies
+        {
+            get => Array.Empty<Type>();
+        }
 
         internal ADisplayPlugin Instanciate()
         {
