4024448a5a6853c184d3a2eee4b158f568c5129f
Xwilarg <xwilarg@yahoo.fr>
2021-11-17 11:39:13 +0100

    Add PathInputText preference

---
+++ Luciol.Plugin/Preference/Impl/PathInputTextPreference.cs
@@ -0,0 +1,33 @@
+﻿using Avalonia.Controls;
+using System.IO;
+
+namespace Luciol.Plugin.Preference
+{
+    /// <summary>
+    /// Input text preference representing a path on the computer
+    /// </summary>
+    public class PathInputTextPreference : APreference<TextBox, string>
+    {
+        public PathInputTextPreference(string key, string name, string defaultValue) : base(key, name, defaultValue)
+        { }
+
+        protected override string ComponentValue
+        {
+            get
+            {
+                if (_component.Text == null || !File.Exists(_component.Text))
+                {
+                    return default;
+                }
+                return Value;
+            }
+            set
+            {
+                if (File.Exists(value))
+                {
+                    _component.Text = value.ToString();
+                }
+            }
+        }
+    }
+}
