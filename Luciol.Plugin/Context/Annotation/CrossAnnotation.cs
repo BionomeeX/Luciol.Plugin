31a9a6bb75d6bc40d0c69b3a9d3764f8ebb169f9
Xwilarg <xwilarg@yahoo.fr>
2021-11-25 16:47:43 +0100

    Change color class used for annotation

--- Luciol.Plugin/Context/Annotation/CrossAnnotation.cs
+++ Luciol.Plugin/Context/Annotation/CrossAnnotation.cs
@@ -1,7 +1,7 @@
-﻿using System;
+﻿using Luciol.Plugin.Preference;
+using System;
 using System.ComponentModel;
 using System.Diagnostics.CodeAnalysis;
-using System.Drawing;
 
 namespace Luciol.Plugin.Context.Annotation
 {
