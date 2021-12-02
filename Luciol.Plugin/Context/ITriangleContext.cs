e555d1e9b4cdc072dee27e528967338011b9094a
Xwilarg <xwilarg@yahoo.fr>
2021-12-02 04:26:44 +0100

    Add ITriangleContext

---
+++ Luciol.Plugin/Context/ITriangleContext.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+
+namespace Luciol.Plugin.Context.Annotation
+{
+    public interface ITriangleContext
+    {
+        public IEnumerable<string> GetOptions();
+        public void Enable(string key);
+        public void Disable(string key);
+        public void GetValue(float value, float maxValue);
+    }
+}
