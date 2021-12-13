f46cded7c16e3e8fa5ccfa68862ba68b7472623e
Xwilarg <xwilarg@yahoo.fr>
2021-12-13 22:24:57 +0100

    Add Annotation global settings

---
+++ Luciol.Plugin/Preference/Global/AnnotationSettings.cs
@@ -0,0 +1,24 @@
+﻿using System.Text.Json.Serialization;
+
+namespace Luciol.Plugin.Preference.Global
+{
+    public class AnnotationSettings : Settings
+    {
+        public AnnotationSettings() : base(_export)
+        { }
+
+        /// <summary>
+        /// Set of colors used for Manhattan graphs
+        /// </summary>
+        [JsonIgnore]
+        public CheckBoxPreference AlwaysDisplayAnnotations
+        {
+            get => (CheckBoxPreference)_export[0];
+        }
+
+        private static readonly IPreferenceExport[] _export = new IPreferenceExport[]
+        {
+            new CheckBoxPreference("alwaysDisplayAnnotations", "Always display annotations", false)
+        };
+    }
+}
