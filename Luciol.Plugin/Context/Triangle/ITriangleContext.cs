60ce622c46a26a1a64eb5f69a37e26c1e29aeae6
Xwilarg <xwilarg@yahoo.fr>
2022-01-04 13:51:11 +0100

    Improve ITriangleContext interface

--- Luciol.Plugin/Context/Triangle/ITriangleContext.cs
+++ Luciol.Plugin/Context/Triangle/ITriangleContext.cs
@@ -9,6 +9,7 @@ namespace Luciol.Plugin.Context.Triangle
         public IEnumerable<string> GetOptions();
         public void Enable(string key);
         public void Disable(string key);
+        public bool IsEnabled(string key);
         public Task<int[][]> GetScreenBufferAsync(int layer, int screenWidth, int screenHeight, int xOffset, int yOffset, int pixelSize);
     }
 }
