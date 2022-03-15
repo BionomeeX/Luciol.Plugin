44e27d6ac7955bc9255cbfc758a2bdea82362b19
Xwilarg <xwilarg@yahoo.fr>
2022-03-15 15:10:52 +0100

    Add global exception handling

--- Luciol.Plugin/APluginViewModel.cs
+++ Luciol.Plugin/APluginViewModel.cs
@@ -1,9 +1,18 @@
 ﻿using ReactiveUI;
+using System.Reactive;
 
 namespace Luciol.Plugin
 {
     public abstract class APluginViewModel : ReactiveObject
     {
+        public APluginViewModel()
+        {
+            RxApp.DefaultExceptionHandler = Observer.Create<Exception>((e) =>
+            {
+                ;
+            });
+        }
+
         public virtual void Init(ADisplayPlugin plugin)
         { }
     }
